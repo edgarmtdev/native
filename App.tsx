c2be62c8ad100cf06e0baee9de15e80d0499c112
edgarmtdev <edgarmontiel961@gmail.com>
2023-08-30 10:23:00 -0600

    Initial project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  Image,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Images</Text>
+      <Image
+        source={{ uri: "https://picsum.photos/200/200" }}
+        style={styles.image}
+      />
+      <TouchableOpacity
+        onPress={() => Alert.alert("Nice!!")}
+        style={styles.button}
+      >
+        <Text>Press me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +29,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#212121",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  title: { color: "#fff", fontSize: 32 },
+  image: { width: 200, height: 200 },
+  button: {
+    margin: 10,
+    padding: 6,
+    backgroundColor: "white",
+    borderRadius: 999,
   },
 });
